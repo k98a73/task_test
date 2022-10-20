8b74a3d05d6be22e506f7b341dcdfa2e8170be4f
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-10-20 17:05:58 +0900

    Laravel MVCモデルの記述方法 1

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TestController extends Controller
-{
-    //
+class TestController extends Controller {
+  public function index() {
+    return view('tests.test');
+  }
 }
