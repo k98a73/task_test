21d560fb9d3416b108dafd051191a3e4a5c4998f
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-10-24 13:06:55 +0900

    認証 ( Vite と Laravel Breeze)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +14,11 @@ use App\Http\Controllers\TestController;
 */
 
 Route::get('/', function () {
-  return view('welcome');
+    return view('welcome');
 });
 
-Route::get('tests/test', [TestController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
